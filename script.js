26c7453dd3191ee37ceb519c3c126ea448b6584a
Spimiki <spimiki@gmail.com>
2023-10-21 14:50:20 +0200

    added functionality

---
+++ script.js
@@ -0,0 +1,86 @@
+const result = document.getElementById("result");
+
+const clearButton = document.getElementById("clearButton");
+
+const plusMinusButton = document.getElementById("plusMinusButton");
+
+const percentButton = document.getElementById("percentButton");
+
+const zeroButton = document.getElementById("zeroButton");
+const oneButton = document.getElementById("oneButton");
+const twoButton = document.getElementById("twoButton");
+const threeButton = document.getElementById("threeButton");
+const fourButton = document.getElementById("fourButton");
+const fiveButton = document.getElementById("fiveButton");
+const sixButton = document.getElementById("sixButton");
+const sevenButton = document.getElementById("sevenButton");
+const eightButton = document.getElementById("eightButton");
+const nineButton = document.getElementById("nineButton");
+
+const plusButton = document.getElementById("plusButton");
+const sumButton = document.getElementById("sumButton");
+const minusButton = document.getElementById("minusButton");
+const timesButton = document.getElementById("timesButton");
+const divideButton = document.getElementById("divideButton");
+
+const dotButton = document.getElementById("dotButton");
+
+let currentValue = 0;
+
+let array = [];
+
+clearButton.addEventListener("click", () => {
+  reset()
+});
+
+plusMinusButton.addEventListener("click", () => {
+  result.innerHTML *= -1;
+});
+
+sumButton.addEventListener("click", () => sum());
+
+plusButton.addEventListener("click", () => handleFunctionClick(`+`));
+minusButton.addEventListener("click", () => handleFunctionClick(`-`));
+timesButton.addEventListener("click", () => handleFunctionClick(`*`));
+divideButton.addEventListener("click", () => handleFunctionClick(`/`));
+
+zeroButton.addEventListener("click", () => setValue(0));
+oneButton.addEventListener("click", () => setValue(1));
+twoButton.addEventListener("click", () => setValue(2));
+threeButton.addEventListener("click", () => setValue(3));
+fourButton.addEventListener("click", () => setValue(4));
+fiveButton.addEventListener("click", () => setValue(5));
+sixButton.addEventListener("click", () => setValue(6));
+sevenButton.addEventListener("click", () => setValue(7));
+eightButton.addEventListener("click", () => setValue(8));
+nineButton.addEventListener("click", () => setValue(9));
+
+dotButton.addEventListener("click", () => setValue("."));
+
+function setValue(value) {
+  result.innerHTML == 0
+    ? (result.innerHTML = value)
+    : (result.innerHTML += value);
+}
+
+function handleFunctionClick(operation) {
+  array.push(parseFloat(result.innerHTML));
+  array.push(operation);
+  reset();
+  currentValue = 0;
+  for (let i = 0; i < array.length; i++) {
+    currentValue += array[i];
+  }
+  console.log(array);
+}
+
+function sum() {
+  currentValue += `${result.innerHTML}`;
+  currentValue = eval(currentValue);
+  result.innerHTML = /[0-9]/.test(currentValue) ? currentValue : "error";
+  array = [];
+}
+
+function reset() {
+  result.innerHTML = 0;
+}
